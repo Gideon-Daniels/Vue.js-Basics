f8ac07704248afce99068c0ac0c3718c998c106d
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-10-07 15:50:00 +0200

    Update-added url

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = Vue.createApp({
   data(){
       return {
           showBooks : false,
+          url : "www.netninja.co.uk",
           books : [
-              { title: 'name of the wind', author: 'patric rothfuss' },
-              { title: 'the way of kings', author: 'brandson sanderson' },
-              { title: 'the final emppire', author: 'brandon sanderson' },
+              { title: 'name of the wind', author: 'patric rothfuss', img: 'assets/1.jpg', isFav: true },
+              { title: 'the way of kings', author: 'brandson sanderson', img: 'assets/2.jpg', isFav: false  },
+              { title: 'the final emppire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true  },
+
           ]
       }
   },
